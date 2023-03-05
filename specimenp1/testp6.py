758ccc52c009cb4dd878558f9f55e7bf88ac397e
mashiro00o <125548786+mashiro00o@users.noreply.github.com>
2023-03-05 17:28:28 +0800

    Create testp6.py

---
+++ specimenp1/testp6.py
@@ -0,0 +1,16 @@
+a = str(input())
+b = str(input())
+c = str(input())
+d = str(input())
+e = str(input())
+
+if a == ('Ellie'):
+  print('1st')
+elif b == ('Ellie'):
+  print('2nd')
+elif c == ('Ellie'):
+  print('3rd')
+elif d == ('Ellie'):
+  print('4th')
+else:
+  print('5th')
