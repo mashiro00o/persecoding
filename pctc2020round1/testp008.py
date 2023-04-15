71ab5f239e893eb154b23925f8b10f4fe88c3ceb
mashiro00o <125548786+mashiro00o@users.noreply.github.com>
2023-04-15 20:31:11 +0800

    Update testp008.py

--- pctc2020round1/testp008.py
+++ pctc2020round1/testp008.py
@@ -1,1 +1,10 @@
+days_until_fruit_day = int(input())
+eat_by_days = []
+while True:
+    eat_by_day = int(input())
+    if eat_by_day == -1:
+        break
+    eat_by_days.append(eat_by_day)
 
+edible_fruits = sum(1 for eat_by_day in eat_by_days if eat_by_day >= days_until_fruit_day)
+print(edible_fruits)
