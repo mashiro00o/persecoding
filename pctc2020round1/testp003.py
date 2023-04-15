88b98b23bdcdc0e923f048c0e999c964c8512d69
mashiro00o <125548786+mashiro00o@users.noreply.github.com>
2023-04-15 20:00:08 +0800

    Update testp003.py

--- pctc2020round1/testp003.py
+++ pctc2020round1/testp003.py
@@ -1,1 +1,11 @@
+food1 = input()
+food2 = input()
+food3 = input()
+food4 = input()
 
+shopping_list = ", ".join([food1, food2, food3, food4])
+
+dashes = '-' * len(shopping_list)
+
+print(shopping_list)
+print(dashes)
