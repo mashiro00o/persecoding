c270d26aa2ef11ff5d67f307c3e1e36e4a983934
mashiro00o <125548786+mashiro00o@users.noreply.github.com>
2023-04-15 19:36:50 +0800

    Update testp001.py

--- pctc2020round1/testp001.py
+++ pctc2020round1/testp001.py
@@ -1,1 +1,10 @@
+num_starfish = int(input())
+num_spider = int(input())
+num_scorpion = int(input())
 
+num_starfisheyes = num_starfish * 5
+num_spidereyes = num_spider * 8
+num_scorpioneyes = num_scorpion * 12
+
+total_eyes = num_starfisheyes + num_spidereyes + num_scorpioneyes
+print(total_eyes)
